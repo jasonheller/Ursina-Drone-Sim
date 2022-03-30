a39a8309b9395f3cd17e993b212b284d9b374368
Jason Heller <79803619+jasonheller@users.noreply.github.com>
2022-03-30 19:39:25 +0200

    updates

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-# Ursina-Drone-Sim
- A drone simulator made in the ursina engine using python
 
-# Requirements
- The ursina engine is required! ``pip install ursina``
+# Ursina-Drone-Sim
+	
+	A drone simulator made in the ursina engine using python
+
+### Requirements
+
+	The ursina engine is **required**! `pip install ursina`
  
-# License
- Do-What-You-Like License, please credit me tho :)
+### Features
+
+	The drone simulator currently features only a small variety of things. (Listed below)
+	- [X] 3 different Flightmodes  
+	- [ ] 
+	- [ ] 
+
+### Future Ideas
+
+	These things listed below might me added in the Future!
+	- [ ] At least 1 map with obstacles
+	- [ ] Better POV Camera system
+	- [ ] Online Mode?
+
+### License
+
+	**Do-What-You-Like License**, please credit me tho :)
--- drone.py
+++ drone.py
@@ -16,46 +16,52 @@ class Drone(Entity):
             )
         self.speed = 0
         
-        self.fmode = ["Race", "Assisted"]
+        self.fmode = ["Race", "Assisted", "Cinematic"]
         self.acceleration = 0.25
         self.droneSpeed = .5
         self.droneForwardSpeed = 35
         self.index = 0
-        self.autoHold = False
 
         self.alt = Text(text='Altitude: '+str(self.y), x=0, y=0.49)
         self.spe = Text(text='Speed: '+str(self.speed), x=0, y=0.46)
         self.md = Text(text='Mode: '+str(self.fmode[self.index]), x=0, y=0.43)
-        self.hold = Text(text='Autohold: '+str(self.autoHold), x=0, y=0.40)
+
+    def reset(self):
+        self.y = 1 
+        self.speed = 0
+        self.rotation_x = 0
+        self.rotation_z = 0
 
     def update(self):
-        
+
         self.alt.text = "Altitude: "+str(math.floor(self.y))+" Meters"
         self.spe.text = "Speed: "+str(self.speed)
         self.md.text = "Mode: "+str(self.fmode[self.index])
-        self.hold.text = "Autohold: "+str(self.autoHold)
 
-        # Race Modes
-        if self.fmode[0] == "Race" and self.autoHold == False:
+        # Modes
+        # Race
+        if self.index == 0:
             self.speed = min(self.speed, self.droneSpeed)
             self.y += self.speed
-        elif self.fmode == "Assisted" and self.autoHold == False:
+        # Assisted
+        elif self.index == 1:
             self.speed = min(self.speed, self.droneSpeed)
             self.y += self.speed
-
-           
-        if held_keys['space']:
-            self.autoHold = True
-        if self.autoHold == True:
+        # Cine
+        elif self.index == 2:
             self.y = self.y
+            self.rotation_x = 0
+            self.rotation_z = 0
+        
+        # go up
+        if held_keys['space']:
+            self.y += self.speed
             self.rotation_x = 0
             self.rotation_z = 0
         
         # flight_mode
         if held_keys['x']:
             self.index += 1
-            self.rotation_x = -8
-            self.z -= self.acceleration * self.droneForwardSpeed* time.dt
         if self.index >= len(self.fmode):
             self.index = 0
         
@@ -72,18 +78,16 @@ class Drone(Entity):
             self.rotation_x = -8
             self.z -= self.acceleration * self.droneForwardSpeed* time.dt
 
-        if held_keys['a']: 
-            self.x -= self.acceleration * self.droneForwardSpeed * time.dt
-            self.rotation_z = -8
+        if self.y < 0:
+            self.reset()
+        
+        #if held_keys['a']: 
+        #    self.x -= self.acceleration * self.droneForwardSpeed * time.dt
+        #    self.rotation_z = -8
 
-        if held_keys['a']: 
-            self.x += self.acceleration * self.droneForwardSpeed * time.dt
-            self.rotation_z = 8
+        #if held_keys['a']: 
+        #    self.x += self.acceleration * self.droneForwardSpeed * time.dt
+        #    self.rotation_z = 8       
 
         # reset
-        if self.y < 0:
-            self.y = 1 
-            self.speed = 0
-            self.rotation_x = 0
-            self.rotation_z = 0
 
